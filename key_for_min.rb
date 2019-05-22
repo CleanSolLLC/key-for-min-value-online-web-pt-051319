b912958d770aecc721638a41628aade5e9177ab0
Mark May <mamark90@gmail.com>
2019-05-22 16:43:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
  min_val = -1
  min_key = ""
  name_hash.each do |key, val|
-   min_val = val if min_val == -1
+   min_val, min_key = val, key if min_val == -1
    min_val, min_key = val, key if min_val > val
 
  end
