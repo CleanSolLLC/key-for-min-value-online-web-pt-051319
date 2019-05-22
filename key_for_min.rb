535673776889e7059f25629e413cac78738ada26
Mark May <mamark90@gmail.com>
2019-05-22 16:42:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,12 @@
 def key_for_min_value(name_hash)
   require 'pry'
   return nil if name_hash.length == 0
- min_val = 1000
+ #min_val = 1000
+ min_val = -1
  min_key = ""
  name_hash.each do |key, val|
- min_val, min_key = val, key if min_val > val
+   min_val = val if min_val == -1
+   min_val, min_key = val, key if min_val > val
 
  end
  min_key
